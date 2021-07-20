a311500edc46db0d3cd8bffc90b7e359ccd65d5f
Evan Williams <evan.b.williams95@gmail.com>
2021-07-20 14:36:55 -0500

    finished walkthrough on controllers exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -8,41 +8,41 @@ public class PostController {
     @GetMapping("/posts")
     @ResponseBody
     public String posts() {
-        return "/posts/index";
+        return "view all posts";
     }
 
     @GetMapping("/posts/{id}")
     @ResponseBody
-    public String postId(){
-        return "/posts/show";
+    public String singlePost(@PathVariable long id){
+        return "View an individual post";
     }
 
 //    Request mapping for explicit path and method
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
-    public String createGet(){
-        return "Form for create a blog post";
-    }
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String createPost(){
-        return "Form for create a blog post";
-    }
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String createForm(){
+//        return "Form for create a blog post";
+//    }
+//
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+//    @ResponseBody
+//    public String createPost(){
+//        return "Form for create a blog post";
+//    }
 
 
 
 //    Get and Post mapping direct annotation
-//    @GetMapping("/posts/create")
-//    @ResponseBody
-//    public String createGet(){
-//        return "Form for create a blog post";
-//    }
-//
-//    @PostMapping("/posts/create")
-//    @ResponseBody
-//    public String createPost(){
-//        return "Create post here";
-//    }
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createForm(){
+        return "Form to create a blog post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "Create post here";
+    }
 
 }
