aace793b6b79d482864c525b2ce2ea34b45e9e04
Evan Williams <evan.b.williams95@gmail.com>
2021-09-07 14:12:06 -0500

    changed post index and moved edit and delete buttons only to posts that user created in show

--- src/main/java/com/codeup/springblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post,Long> {
 
     Post findById(long id);
-    void deleteById(long id);
 
     void delete(Post post);
     Post save(Post post);
