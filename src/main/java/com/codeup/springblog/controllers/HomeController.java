9792ce7e7edac60116baa969ef2a4860ccde8d58
Evan Williams <evan.b.williams95@gmail.com>
2021-09-09 16:20:54 -0500

    created search directory with results and index html

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,7 +1,16 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
+import jdk.jfr.Event;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -9,5 +18,23 @@ public class HomeController {
     @GetMapping("/")
     public String welcome() {
         return "home";
+    }
+
+    @GetMapping("/search")
+    public String getSearchForm() {
+        return "search/index";
+    }
+
+//    @PostMapping("/search")
+//    public String makeSearch(Model model, @RequestParam(name = "search-params") String params, @RequestParam(name = "query") String query) {
+//        List<Post> searchResults;
+//        searchResults = postD.findAllEventsByTitleOrLocationContaining(query);
+//        model.addAttribute("searchResults", searchResults);
+//        return "search/results";
+//    }
+
+    @GetMapping("/search/results")
+    public String showResults(@ModelAttribute(name = "searchResults") ArrayList<Event> searchResults, @ModelAttribute(name = "searchResults2") ArrayList<Organization> searchResults2) {
+        return "search/results";
     }
 }
