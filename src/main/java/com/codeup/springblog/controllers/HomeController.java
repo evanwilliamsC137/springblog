6218dd9f90fe772ef82ad221c82de7a1c9d67dd6
Evan Williams <evan.b.williams95@gmail.com>
2021-09-10 11:35:57 -0500

    search functionality is now working

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repositories.PostRepository;
 import jdk.jfr.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.util.ArrayList;
@@ -14,6 +16,12 @@ import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private PostRepository postsDao;
+
+    public HomeController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
 
     @GetMapping("/")
     public String welcome() {
@@ -25,16 +33,16 @@ public class HomeController {
         return "search/index";
     }
 
-//    @PostMapping("/search")
-//    public String makeSearch(Model model, @RequestParam(name = "search-params") String params, @RequestParam(name = "query") String query) {
-//        List<Post> searchResults;
-//        searchResults = postD.findAllEventsByTitleOrLocationContaining(query);
-//        model.addAttribute("searchResults", searchResults);
-//        return "search/results";
-//    }
+    @PostMapping("/search")
+    public String makeSearch(Model model, @RequestParam(name = "query") String query) {
+        List<Post> searchResults;
+        searchResults = postsDao.findAllPostsContaining(query);
+        model.addAttribute("searchResults", searchResults);
+        return "search/results";
+    }
 
     @GetMapping("/search/results")
-    public String showResults(@ModelAttribute(name = "searchResults") ArrayList<Event> searchResults, @ModelAttribute(name = "searchResults2") ArrayList<Organization> searchResults2) {
+    public String showResults(@ModelAttribute(name = "searchResults") ArrayList<Post> searchResults) {
         return "search/results";
     }
 }
--- src/main/java/com/codeup/springblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepository.java
@@ -2,6 +2,9 @@ package com.codeup.springblog.repositories;
 
 import com.codeup.springblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 
@@ -9,6 +12,9 @@ public interface PostRepository extends JpaRepository<Post,Long> {
 
     void delete(Post post);
     Post save(Post post);
+
+    @Query("from Post p where p.title like %:query%")
+    List<Post> findAllPostsContaining(String query);
 
     Post findByTitle(String post_to_be_deleted);
 //    void editById(long id, String title, String body);
